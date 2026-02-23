8bbedd3b093dddd1b24b7735916ed392dbe4166c
martinsinnona <martinsinnona@outlook.es>
2026-02-23 18:31:38 -0300

    Updated Github button and Report pdf

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -54,7 +54,7 @@ export default function Hero({ heroVariant }: any) {
                     Try now
                 </Link>
                 <a
-                    href="https://github.com/LIA-DiTella/VisDecode_demo"
+                    href="https://github.com/LIA-DiTella/visdecode-landing"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="flex items-center justify-center gap-2 rounded-xl sm:rounded-full border border-gray-300 bg-white px-6 sm:px-8 2xl:px-10 3xl:px-14 4xl:px-18 py-2.5 sm:py-3 2xl:py-5 3xl:py-6 4xl:py-8 text-base sm:text-lg 2xl:text-xl 3xl:text-2xl 4xl:text-3xl font-medium shadow-sm transition w-full hover:bg-gray-100 cursor-pointer active:scale-95"
